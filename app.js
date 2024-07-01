1071036e00a3ad91214190bec3ef65c6d35b342f
jasbanza <jason.banfield.za@gmail.com>
2024-07-01 17:30:50 +0200

    message sending works

--- app.js
+++ app.js
@@ -2,118 +2,185 @@ const express = require('express');
 const dotenv = require('dotenv');
 const fetch = require('node-fetch');
 
-// Load environment variables from .env file
+// Load environment variables from.env file
 dotenv.config();
 
 // Create an Express application
 const app = express();
 
-// Set up a route for the home page
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+/**
+ * Serve the home page.
+ */
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
-    // it should also serve the public folder
-    app.use(express.static('public'));
 });
 
-// handle request to get all tickets
+/**
+ * Fetch and return tickets for a specific department.
+ * @param {string} departmentId - The ID of the department to filter tickets by.
+ * @returns Tickets filtered by the specified department ID.
+ */
 app.get('/tickets', async (req, res) => {
-    // get the departmentId from the query parameters
     const departmentId = req.query.departmentId;
     const url = `https://api.helpcrunch.com/v1/chats/search`;
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${process.env.HC_API_KEY}`
-        },
-        body: JSON.stringify({
-            "filter": [
-                {
-                    "field": "chats.department",
-                    "operator": "=",
-                    "value": departmentId
-                }
-            ],
-            "sort": "chats.createdAt",
-            "order": "desc",
-            "offset": 0,
-            "limit": 100
-        })
-    });
-    const data = await response.json();
-    res.json(data);
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Authorization': `Bearer ${process.env.HC_API_KEY}` },
+            body: JSON.stringify({
+                filter: [{ field: "chats.department", operator: "=", value: departmentId }],
+                sort: "chats.createdAt",
+                order: "desc",
+                offset: 0,
+                limit: 100
+            })
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Failed to fetch tickets:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
+/**
+ * Return information about the application.
+ * @returns Application information including the department name.
+ */
 app.get('/app-info', async (req, res) => {
-    res.json({departmentName: process.env.DEPARTMENT_NAME});
+    res.json({ departmentName: process.env.DEPARTMENT_NAME });
 });
 
-// get ticket by department id from .env
+/**
+ * Fetch and return tickets for the department specified in the environment variable.
+ * @returns Tickets filtered by the department ID from the environment variable.
+ */
 app.get('/department-tickets', async (req, res) => {
     const url = `https://api.helpcrunch.com/v1/chats/search`;
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${process.env.HC_API_KEY}`
-        },
-        body: JSON.stringify({
-            "filter": [
-                {
-                    "field": "chats.department",
-                    "operator": "=",
-                    "value": process.env.DEPARTMENT_ID
-                }
-            ],
-            "sort": "chats.createdAt",
-            "order": "desc",
-            "offset": 0,
-            "limit": 100
-        })
-    });
-    const data = await response.json();
-    res.json(data);
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Authorization': `Bearer ${process.env.HC_API_KEY}` },
+            body: JSON.stringify({
+                filter: [{ field: "chats.department", operator: "=", value: process.env.DEPARTMENT_ID }],
+                sort: "chats.createdAt",
+                order: "desc",
+                offset: 0,
+                limit: 100
+            })
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Failed to fetch department tickets:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
-// handle request to get a single ticket
+/**
+ * Fetch and return a single ticket by its ID.
+ * @param {string} id - The ID of the ticket to retrieve.
+ * @returns The requested ticket.
+ */
 app.get('/ticket/:id', async (req, res) => {
     const url = `https://api.helpcrunch.com/v1/chats/${req.params.id}`;
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${process.env.HC_API_KEY}`
-        }
-    });
-    const data = await response.json();
-    res.json(data);
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Authorization': `Bearer ${process.env.HC_API_KEY}` }
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Failed to fetch ticket:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
-// handle request to get all messages for a chat
+/**
+ * Fetch and return all messages for a specific chat.
+ * @param {string} id - The ID of the chat to retrieve messages for.
+ * @returns Messages for the specified chat.
+ */
 app.get('/chats/:id/messages', async (req, res) => {
     const chatId = req.params.id;
     const url = `https://api.helpcrunch.com/v1/chats/${chatId}/messages`;
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${process.env.HC_API_KEY}`
-        }
-    });
-    const data = await response.json();
-    res.json(data);
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Authorization': `Bearer ${process.env.HC_API_KEY}` }
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Failed to fetch chat messages:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
-// handle request to get all departments
+/**
+ * Send a message to the active ticket.
+ * @param {string} message - The message to send.
+ * @returns The response from the API.
+ */
+app.post('/chats/:id/sendMessage', async (req, res) => {
+    const chatId = req.params.id;
+
+    // todo: check the HC api for the correct endpoint implementation info
+
+
+    const url = `https://api.helpcrunch.com/v1/messages`;
+
+    // get message from request body
+    const messageText = req.body.text;
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                // 'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.HC_API_KEY}`
+            },
+            body: JSON.stringify({
+                text: messageText,
+                chat: chatId,
+                // "markdownText": "Can you can a **can** as a canner can can a **can**",
+                agent: process.env.HC_AGENT_ID,
+                type: "message"
+            })
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Failed to send message:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+/**
+ * Fetch and return all departments.
+ * @returns List of all departments.
+ */
 app.get('/departments', async (req, res) => {
     const url = `https://api.helpcrunch.com/v1/departments`;
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${process.env.HC_API_KEY}`
-        }
-    });
-    const data = await response.json();
-    res.json(data);
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Authorization': `Bearer ${process.env.HC_API_KEY}` }
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Failed to fetch departments:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
-            
-
 
 // Start the server
 const port = process.env.PORT || 3000;
--- public/index.js
+++ public/index.js
@@ -1,62 +1,46 @@
-// Get the reference to the button and output div
-// const btnGetTickets = document.getElementById('btnGetTickets');
-
-// const selectDepartments = document.getElementById('selectDepartments');
-
-// // Add event listener to the buttons
-// document.getElementById('btnGetTickets').addEventListener('click', () => {
-//     getTicketsByDepartmentId(selectDepartments.value);
-// });
-
-// document.getElementById('btnGetSquidTickets').addEventListener('click', async () => {
-//     getTicketsByDepartmentId(142160);
-// });
-
-
-
-document.getElementById('btnRefreshTickets').addEventListener('click', async () => {
-    refreshTicketsForDepartment();
-});
-
-// document.getElementById('btnGetDepartments').addEventListener('click', () => {
-//     getDepartments();
-// });
-// document.getElementById("statusSelector").addEventListener("change", statusSelectorCallback);
-
+/**
+ * Event listener for DOMContentLoaded event to initialize the application.
+ */
 document.addEventListener('DOMContentLoaded', init);
 window.activeTicketId = null;
+window.lastActiveTicketId = null;
 
+/**
+ * Initialization function triggered on DOMContentLoaded event.
+ * Fetches app info, gets tickets for the department, and sets up intervals for refreshing tickets.
+ */
 async function init() {
     getAppInfo();
     getTicketsForDepartment();
     setInterval(refreshTicketsForDepartment, 30000);
-    // setInterval(refreshSelectedTicketMessages, 30000);
 }
 
+/**
+ * Refreshes tickets for the department and selected ticket messages.
+ */
+async function refreshTicketsForDepartment() {
+    getTicketsForDepartment();
+    refreshSelectedTicketMessages();
+}
 
-
-
+/**
+ * Refreshes selected ticket messages if an active ticket ID is set.
+ */
 async function refreshSelectedTicketMessages() {
     if (window.activeTicketId) {
         viewTicket(window.activeTicketId);
     }
 }
 
-async function refreshTicketsForDepartment() {
-    getTicketsForDepartment();
-    refreshSelectedTicketMessages();
-}
-
-
+/**
+ * Fetches and displays app information such as the department name.
+ */
 async function getAppInfo() {
     try {
         const response = await fetch('/app-info');
         if (response.ok) {
             const data = await response.json();
             const departmentName = data.departmentName;
-
-            // document.getElementById('departmentName').textContent = departmentName;
-
             const elementsWithClassName = document.getElementsByClassName('department-name');
             Array.from(elementsWithClassName).forEach(element => {
                 element.innerHTML = departmentName;
@@ -67,8 +51,14 @@ async function getAppInfo() {
     }
 }
 
+/**
+ * Performs a fetch operation with a loading mask applied to a specified element.
+ * @param {string} url - The URL to fetch.
+ * @param {object} options - Fetch options.
+ * @param {string} elementIdToMask - The ID of the element to apply the loading mask to.
+ * @returns {Promise<Response>} The fetch response.
+ */
 async function loadingMaskFetch(url, options, elementIdToMask) {
-    // apply loading mask
     const element = document.getElementById(elementIdToMask);
     element.classList.add('loading');
     let response;
@@ -77,40 +67,30 @@ async function loadingMaskFetch(url, options, elementIdToMask) {
     } catch (error) {
         console.error('Error:', error);
     } finally {
-        // remove loading mask
         element.classList.remove('loading');
     }
     return response;
-
 }
 
+/**
+ * Fetches and displays tickets for the department.
+ */
 async function getTicketsForDepartment() {
     try {
         const divTicketList = document.getElementById('divTicketList');
-        // Make a GET request to the root ticket endpoint
-
-
-
         const response = await loadingMaskFetch(`/department-tickets`, {}, 'ticket-list-loader');
-
-        // Check if the request was successful
         if (response.ok) {
-            // Get the response data as JSON
             const data = await response.json();
-
-
-            // for each ticket in the data.data array, display the ticket id and the first message
             divTicketList.innerHTML = data.data
-                .sort((a, b) => b.lastCustomerMessageAt - a.lastCustomerMessageAt) // Sort tickets by last message time (newest to oldest)
+                .sort((a, b) => b.lastCustomerMessageAt - a.lastCustomerMessageAt)
                 .map(ticket => {
-                    return `<div class="ticket-preview ${window.activeTicketId == ticket.id ? "active":""}" data-ticket-id="${ticket.id}" onclick="viewTicket(${ticket.id},'${ticket.customer?.name}')">
+                    return `<div class="ticket-preview ${window.activeTicketId == ticket.id ? "active" : ""}" data-ticket-id="${ticket.id}" onclick="viewTicket(${ticket.id},'${ticket.customer?.name}')">
                     <div class="status-color-bar" data-status=${ticket.status}></div>
                     <div class="ticket-info">
                         <div class="ticket-header">
                             <div class="ticket-id">#${ticket.id}</div>
                             <div class="ticket-assignee">Assignee: ${ticket.assignee?.name}</div>
                         </div>                        
-                        <!--<div class="ticket-status">Status: ${ticket.status} ${ticket.status === 'opened' ? '🟢' : ticket.status === 'closed' ? '🔴' : '🟡'}</div>--> 
                         <div class="ticket-customer">${ticket.customer?.name}</div>
                         <div class="last-message-preview">
                             <div class="last-message-info">
@@ -123,21 +103,23 @@ async function getTicketsForDepartment() {
                             <div class="ticket-created-at">Created ${new Date(ticket.createdAt * 1000).toLocaleString('en-US', { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' })}</div>
                             <div class="ticket-age-ago">${calculateTicketAge(ticket.createdAt)} ago</div>
                         </div>
-                        <!--<div class="ticket-department">Department: ${ticket.department?.name}</div>-->
-                        
-                        <!--<div class="ticket-last-customer-message-at">Last Customer Message At: ${new Date(ticket.lastCustomerMessageAt * 1000).toLocaleString('en-US', { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' })}</div>-->
                     </div>
                 </div>`;
                 }).join('');
         } else {
-            // Display an error message if the request was not successful
             divTicketList.textContent = 'Error: ' + response.status;
         }
     } catch (error) {
-        // Display an error message if there was an error with the request
         divTicketList.textContent = 'Error: ' + error.message;
     }
 }
+
+/**
+ * Truncates text to a maximum length.
+ * @param {string} text - The text to truncate.
+ * @param {number} maxLength - The maximum length of the text.
+ * @returns {string} The truncated text.
+ */
 function truncateText(text, maxLength) {
     if (text.length > maxLength) {
         return text.substring(0, maxLength) + '...';
@@ -145,6 +127,11 @@ function truncateText(text, maxLength) {
     return text;
 }
 
+/**
+ * Calculates the age of a ticket based on its creation timestamp.
+ * @param {number} createdAt - The creation timestamp of the ticket.
+ * @returns {string} The age of the ticket in days, hours, and minutes.
+ */
 function calculateTicketAge(createdAt) {
     const now = new Date();
     const created = new Date(createdAt * 1000);
@@ -155,78 +142,145 @@ function calculateTicketAge(createdAt) {
     return `${diffInDays} days, ${diffInHours} hours, ${diffInMinutes} minutes`;
 }
 
+// Note: The getDepartments function was commented out in the original code and thus not included here for brevity.
 
-async function getDepartments() {
-    try {
-        // Make a GET request to the root departments endpoint
-        const response = await fetch('/departments');
-
-        // Check if the request was successful
-        if (response.ok) {
-            // Get the response data as JSON
-            const data = await response.json();
-
-            // only show the id and name property for each item in the data.data array
-            const departments = data.data.map(department => {
-                return {
-                    id: department.id,
-                    name: department.name
-                };
-            });
-
-            //update selectDepartments with the departments
-            selectDepartments.innerHTML = departments.map(department => {
-                return `<option value="${department.id}">${department.name}</option>`;
-            }).join('');
-
-        } else {
-            // Display an error message if the request was not successful
-            divDepartmentsResponse.textContent = 'Error: ' + response.status;
-        }
-    } catch (error) {
-        // Display an error message if there was an error with the request
-        divDepartmentsResponse.textContent = 'Error: ' + error.message;
-    }
-}
-
-
-
+/**
+ * Displays detailed information about a selected ticket.
+ * @param {number} ticketId - The ID of the ticket to display.
+ * @param {string} customerName - The name of the customer associated with the ticket.
+ */
 function viewTicket(ticketId, customerName = "User") {
+    // Check if the current ticketId is the same as the last active ticketId
+    if (window.activeTicketId !== ticketId) {
+        // If not, reset the active state of all ticket previews
+        const ticketPreviews = document.getElementsByClassName('ticket-preview');
+        Array.from(ticketPreviews).forEach(ticketPreview => {
+            ticketPreview.classList.remove('active');
+        });
+        // Set the current ticketId as the active one
+        window.activeTicketId = ticketId;
+        document.querySelector(`.ticket-preview[data-ticket-id="${ticketId}"]`).classList.add('active');
+    }
 
-    // set the active ticket id
-    window.activeTicketId = ticketId;
-
-    // remove all other active (hightlighted) ticket classes
-    const ticketPreviews = document.getElementsByClassName('ticket-preview');
-    Array.from(ticketPreviews).forEach(ticketPreview => {
-        ticketPreview.classList.remove('active');
-    });
-    // set the ticket preview to active by the data-ticket-id attribute
-    document.querySelector(`.ticket-preview[data-ticket-id="${ticketId}"]`).classList.add('active');
-
+    // Store the current highest message ID if available
+    let lastMessageId = null;
+    const existingMessages = document.querySelectorAll('.message');
+    if (existingMessages.length > 0) {
+        lastMessageId = existingMessages[existingMessages.length - 1].getAttribute('data-message-id');
+    }
 
     loadingMaskFetch(`/chats/${ticketId}/messages`, {}, 'ticket-messages-loader')
         .then(response => response.json())
         .then(data => {
-            // Display the messages in the divTicketMessages
-            document.getElementById('divTicketMessages').innerHTML = data.data.map(message => {
-                if (validateUUID(message.text)) {
-                    message.text = `<a href="https://ucarecdn.com/${message.text}/" target="_blank"><img src="https://ucarecdn.com/${message.text}/" alt="Image"></a>`;
+            const divTicketMessages = document.getElementById('divTicketMessages');
+            const messagesHTML = data.data.reverse().map(message => {
+                let isNewMessage = false;
+                // Highlight new messages only if the ticketId has not changed
+
+                if (message.from !== 'agent' && window.lastActiveTicketId == ticketId && lastMessageId && message.id > lastMessageId) {
+                    isNewMessage = true;
                 }
-                return `<div class="message ${message.from === 'agent' ? (message?.agent?.name === undefined ? 'system' : 'agent') : 'customer'} ">
-                            <div class="message-sender">${message.from === 'agent' ? (message?.agent?.name === undefined ? "System message" : message?.agent?.name) : customerName}</div>
+                if (validateImageURL(message.text)) {
+                    message.text = `<a href="${message.text}" target="_blank"><img src="${message.text}" alt="Image"></a>`;
+                } else
+                    if (validateUUID(message.text)) {
+                        message.text = `<a href="https://ucarecdn.com/${message.text}/" target="_blank"><img src="https://ucarecdn.com/${message.text}/" alt="Image"></a>`;
+                    }
+                let messageClass = `message ${message.from === 'agent' ? (message.agent?.name === undefined ? 'system' : 'agent') : 'customer'} ${isNewMessage ? 'new-message' : ''}`;
+                return `<div class="${messageClass}" data-message-id="${message.id}">
+                            <div class="message-sender">${message.from === 'agent' ? (message.agent?.name === undefined ? "System message" : message.agent.name) : customerName}</div>
                             <div class="message-text">${message.text}</div>
                             <div class="message-timestamp">${new Date(message.updatedAt * 1000).toLocaleString('en-US', { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })}</div>
                         </div>`;
             }).join('');
+
+            divTicketMessages.innerHTML = messagesHTML; // Render messages
+
+            // Update the last active ticketId
+            window.lastActiveTicketId = ticketId;
+
+            // Scroll to the bottom of the messages container
+            requestAnimationFrame(() => {
+                divTicketMessages.scrollTop = divTicketMessages.scrollHeight;
+            });
         })
         .catch(error => {
             console.error('Error:', error);
         });
-
 }
 
+
+
+/**
+ * Validates if a given string is a URL to an image.
+ * @param {string} text - The string to validate.
+ * @returns {string} The image URL if valid, null otherwise.
+ */
+function validateImageURL(text) {
+    const regex = /https:\/\/ucarecdn\.com\/.*\/image\.(jpg|jpeg|png|gif)/i;
+    const match = text.match(regex);
+    return match ? match[0] : null;
+}
+
+/**
+ * Validates if a given string is a UUID.
+ * @param {string} uuid - The string to validate.
+ * @returns {boolean} True if the string is a valid UUID, false otherwise.
+ */
 function validateUUID(uuid) {
     const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
     return regex.test(uuid);
 }
+
+
+
+// on inputMessage textarea resize, adjust divTicketMessages size:
+document.getElementById('inputMessage').addEventListener('resize', function () {
+    document.getElementById('divTicketMessages').style.height = `calc(100vh - (150px +  ${this.scrollHeight}px))`;
+});
+
+
+/**
+ * Sends a message to the selected ticket.
+ */
+
+async function sendMessage() {
+    const message = document.getElementById('inputMessage').value;
+    if (!window.activeTicketId || !message || message.trim() === '') {
+        return;
+    }
+    try {
+        const response = await fetch(`/chats/${window.activeTicketId}/sendMessage`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ text: message })
+        });
+        if (response.ok) {
+            viewTicket(window.activeTicketId);
+        } else {
+            console.error('Failed to send message:', response.status);
+        }
+    } catch (error) {
+        console.error('Failed to send message:', error);
+        alert('Failed to send message. Please try again.');
+    }
+    finally {
+        document.getElementById('inputMessage').value = '';
+    }
+}
+
+
+// listener to send message on CTRL+Enter key press:
+document.getElementById('inputMessage').addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 'Enter') {
+        sendMessage();
+    }
+});
+
+// listener to send message on click of send button:
+document.getElementById('btnSend').addEventListener('click', sendMessage);
+
+// refresh button
+document.getElementById('btnRefreshTickets').addEventListener('click', refreshTicketsForDepartment);
